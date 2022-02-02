9df1dec33f37fffe73ec4ae27e067e20de297aee
Jahid <jahidhasan.developer@gmail.com>
2022-02-02 10:15:00 +0600

    [Add More Script]

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 function readMore() {
-  const id = document.getElementById("readMore");
-  id.classList.toggle("active");
+  toggle("readMore",false);
+}
+
+function leftMenuToggle(){
+  const id = document.getElementById("left_menu_toggle");
+  id.classList.toggle("left_menu_toggle");
+  const id2 = document.getElementById("main_container");
+  id2.classList.toggle("main_container_toggle");
+}
+
+function toggle(id,more){
+  const elementId = document.getElementById(id);
+  elementId.classList.toggle("active");
+
+  if(more === true){
+    const id = document.getElementById("search_option");
+    id.classList.toggle("d-none");
+  }
 }
