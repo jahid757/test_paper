d1b20907ea736e5ee246397e95f71d4e57542eb3
Jahid <jahidhasan.developer@gmail.com>
2022-02-03 10:59:57 +0600

    [All Done]

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ function leftMenuToggle(){
   id2.classList.toggle("main_container_toggle");
 }
 
-function toggle(id,more){
+function toggle(id,more,id2){
   const elementId = document.getElementById(id);
   elementId.classList.toggle("active");
 
   if(more === true){
-    const id = document.getElementById("search_option");
-    id.classList.toggle("d-none");
+    const id = document.getElementById(id2);
+    id.classList.toggle("active");
   }
 }
