ed65ac9fdbfc510adf44c94c9727935b9ffac3df
Jahid <jahidhasan.developer@gmail.com>
2022-02-26 09:33:41 +0600

    login

--- script.js
+++ script.js
@@ -18,3 +18,15 @@ function toggle(id,more,id2){
     id.classList.toggle("active");
   }
 }
+
+function popUpLoginToggle(id,id2,id3,id4){
+  const id_1 = document.getElementById(id);
+  const id_2 = document.getElementById(id2);
+  const id_3 = document.getElementById(id3);
+  const id_4 = document.getElementById(id4);
+
+  id_1.classList.add("active");
+  id_3.classList.add("d-none");
+  id_2.classList.remove("active");
+  id_4.classList.remove("d-none");
+}
